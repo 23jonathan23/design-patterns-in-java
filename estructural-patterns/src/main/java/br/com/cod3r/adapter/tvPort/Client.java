6aa6cf737552c70841fa49e63e3d420db977dfa7
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-05-08 19:22:41 -0300

    tvPort adapter added

--- estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
@@ -1,10 +1,12 @@
 package br.com.cod3r.adapter.tvPort;
 
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToOldMonitorAdapter;
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToVGAAdapter;
 import br.com.cod3r.adapter.tvPort.devices.Computer;
+import br.com.cod3r.adapter.tvPort.devices.OldMonitor;
 import br.com.cod3r.adapter.tvPort.devices.TV;
 
 public class Client {
-
 	public static void main(String[] args) {
 		Computer pc = new Computer();
 		TV tv = new TV();
@@ -13,10 +15,18 @@ public class Client {
 		
 		System.out.println("------ Monitor ----------");
 		
+		Computer pc2 = new Computer();
+		OldMonitor monitor = new OldMonitor();
+		pc2.connectPort(new HDMIToVGAAdapter(monitor));
+		pc2.sendImageAndSound("English Program", "Nyan cat song");
 
 
 		System.out.println("------ Monitor Class Adapter ----------");
 		
+		Computer pc3 = new Computer();
+		HDMIToOldMonitorAdapter monitorAdaptee = new HDMIToOldMonitorAdapter();
+		pc3.connectPort(monitorAdaptee);
+		pc3.sendImageAndSound("Blue Fruits", "Nyan cat song");
 
 	}
 }
--- estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/adapters/HDMIToVGAAdapter.java
+++ estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/adapters/HDMIToVGAAdapter.java
@@ -1,5 +1,24 @@
 package br.com.cod3r.adapter.tvPort.adapters;
 
-public class HDMIToVGAAdapter {
+import br.com.cod3r.adapter.tvPort.intefaces.HDMI;
+import br.com.cod3r.adapter.tvPort.intefaces.VGA;
 
+public class HDMIToVGAAdapter implements HDMI {
+    private VGA _vga;
+
+    public HDMIToVGAAdapter(VGA vga) {
+        System.out.println("Conecting the HDMI/VGA adapter...");
+        _vga = vga;
+    }
+
+    @Override
+    public void setImage(String image) {
+        System.out.println("Coverting the image from HDMI to VGA");
+        _vga.setImage(image);
+    }
+
+    @Override
+    public void setSound(String sound) {
+        System.out.println("Sorry, We don't work with sound");
+    }
 }
