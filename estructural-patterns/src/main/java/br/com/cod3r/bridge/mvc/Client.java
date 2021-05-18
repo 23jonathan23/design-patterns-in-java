14e3a4a8371087a242ced1aaadca9e20ad1339ee
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-05-18 17:06:52 -0300

    api exemplo bridge pattner implemented

--- estructural-patterns/src/main/java/br/com/cod3r/bridge/mvc/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/bridge/mvc/Client.java
@@ -1,6 +1,13 @@
 package br.com.cod3r.bridge.mvc;
 
+import br.com.cod3r.bridge.mvc.dao.UserOracleDao;
+import br.com.cod3r.bridge.mvc.dao.UserMongoDao;
+import br.com.cod3r.bridge.mvc.dao.UserPostgresDao;
 import br.com.cod3r.bridge.mvc.model.User;
+import br.com.cod3r.bridge.mvc.services.UserRest;
+import br.com.cod3r.bridge.mvc.services.UserService;
+import br.com.cod3r.bridge.mvc.services.UserEJB;
+import br.com.cod3r.bridge.mvc.services.UserSoap;
 
 public class Client {
 
@@ -8,6 +15,13 @@ public class Client {
 	public static void main(String[] args) {
 		User user = new User("user", "user@email.com", "passwd");
 		
-		
+		UserService userRestOracleService = new UserRest(new UserOracleDao());
+		userRestOracleService.save(user);
+
+		UserService userEJBMongoService = new UserEJB(new UserMongoDao());
+		userEJBMongoService.save(user);
+
+		UserService userSoapPostgresService = new UserSoap(new UserPostgresDao());
+		userSoapPostgresService.save(user);
 	}
 }
